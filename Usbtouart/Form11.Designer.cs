16c8ab2f862345bafbe956a525c3f03944db35db
Sasha Savchuk <savchuksasha195@gmail.com>
2018-10-21 16:13:52 +0300

    first point

---
+++ Usbtouart/Form11.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form11));
+            this.bClear = new System.Windows.Forms.Button();
+            this.bSend = new System.Windows.Forms.Button();
+            this.tbTX = new System.Windows.Forms.RichTextBox();
+            this.tbRX = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.mySerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bClear
+            // 
+            this.bClear.Location = new System.Drawing.Point(643, 68);
+            this.bClear.Name = "bClear";
+            this.bClear.Size = new System.Drawing.Size(75, 23);
+            this.bClear.TabIndex = 0;
+            this.bClear.Text = "Clear";
+            this.bClear.UseVisualStyleBackColor = true;
+            this.bClear.Click += new System.EventHandler(this.bClear_Click);
+            // 
+            // bSend
+            // 
+            this.bSend.Location = new System.Drawing.Point(643, 12);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(75, 23);
+            this.bSend.TabIndex = 1;
+            this.bSend.Text = "Send";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.bSend_Click);
+            // 
+            // tbTX
+            // 
+            this.tbTX.Location = new System.Drawing.Point(12, 12);
+            this.tbTX.Name = "tbTX";
+            this.tbTX.Size = new System.Drawing.Size(615, 79);
+            this.tbTX.TabIndex = 2;
+            this.tbTX.Text = "";
+            this.tbTX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbTX_KeyPress);
+            // 
+            // tbRX
+            // 
+            this.tbRX.Location = new System.Drawing.Point(12, 97);
+            this.tbRX.Name = "tbRX";
+            this.tbRX.Size = new System.Drawing.Size(615, 95);
+            this.tbRX.TabIndex = 3;
+            this.tbRX.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(643, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(38, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "IM";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // mySerialPort
+            // 
+            this.mySerialPort.PortName = "COM5";
+            this.mySerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.mySerialPort_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Dev 1 Ch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM 1",
+            "COM 2",
+            "COM 3",
+            "COM 4",
+            "COM 5",
+            "COM 6",
+            "COM 19"});
+            this.comboBox1.Location = new System.Drawing.Point(643, 151);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(45, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 26);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(133, 199);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 27);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(242, 198);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 27);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 21);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Dev 1 OK";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Dev 2 Ch";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(134, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 21);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Dev 2 OK";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(243, 241);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Dev 3 Ch";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(243, 270);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 21);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Dev 3 OK";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(640, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "COM port";
+            // 
+            // Form11
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 301);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.tbRX);
+            this.Controls.Add(this.tbTX);
+            this.Controls.Add(this.bSend);
+            this.Controls.Add(this.bClear);
+            this.Name = "Form11";
+            this.Text = "Form11";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bClear;
+        private System.Windows.Forms.Button bSend;
+        private System.Windows.Forms.RichTextBox tbTX;
+        private System.Windows.Forms.RichTextBox tbRX;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.IO.Ports.SerialPort mySerialPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Usbtouart/Form11.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form11 : Form
+    {
+        
+        public Form11()
+        {
+            InitializeComponent();
+         
+         
+            if (!mySerialPort.IsOpen)
+            {
+                mySerialPort.Open();
+                tbRX.Text = "open :) ";
+            }
+            else
+                tbRX.Text = "busy :( ";
+
+        }
+
+        private string rxString;
+        private int rxInt;
+        private int rxIntcmd;
+        private int rxIntl;
+        private int rxIntl2;
+        private int rxIntl3;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mySerialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            rxInt = mySerialPort.ReadByte();
+            rxIntcmd = mySerialPort.ReadByte();
+            rxIntl = mySerialPort.ReadByte();
+            rxIntl2 = mySerialPort.ReadByte();
+            rxIntl3 = mySerialPort.ReadByte();
+            
+
+            if(rxInt == 0x55)
+            {
+                MessageBox.Show("ok");
+            }
+            else
+            {
+                MessageBox.Show("Fail");
+            }
+            /* 
+             int dataLength = mySerialPort.BytesToRead;
+             byte[] data = new byte[dataLength];
+             int nbrDataRead = mySerialPort.Read(data, 0, dataLength);
+             if (nbrDataRead == 0)
+                 return;
+             */
+            this.Invoke(new EventHandler(displayText));
+
+        }
+
+        private void displayText(object o, EventArgs e)
+        {
+           
+            tbRX.AppendText(rxInt + " ");
+            tbRX.AppendText("\n");
+           // pictureBox1.Show();
+        }
+
+        
+        private void bSend_Click(object sender, EventArgs e)
+        {
+            // mySerialPort.Write(rxInt);
+            mySerialPort.Write(new byte[] { 0x55, 0x03, 0x11, 0x23, 0x23 }, 0, 5);
+        }
+       
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            tbTX.Clear();
+            tbRX.Clear();
+          //  pictureBox1.Hide();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            mySerialPort.Close();
+        }
+
+        private void tbTX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (mySerialPort.IsOpen && checkBox1.Checked)
+            {
+                char[] ch = new char[1];
+                ch[0] = e.KeyChar;
+                mySerialPort.Write(ch, 0, 1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // mySerialPort.Write(tbTX.Text);
+            //  mySerialPort.Write("20");
+            mySerialPort.Write(new byte[] { 0x55, 0x3, 0x11, 0x23, 0x23 }, 0, 5);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
